7f64d744cb5e2a68eb4c2f25218515f88bd313ca
Nik <nik_rink@ukr.net>
2023-10-08 13:01:00 +0300

    Комит в master ветку dz12-spring-nik

---
+++ src/main/java/pro/sky/dz12springnik/Dz12SpringNikApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.dz12springnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz12SpringNikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dz12SpringNikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/dz12springnik/Dz12SpringNikApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.dz12springnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz12SpringNikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
